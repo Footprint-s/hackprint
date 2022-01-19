82925b32c01f9627af513d93b05698095a9ce890
aman162000 <borse.aman@rediffmail.com>
2022-01-19 13:57:05 +0530

    Uodated

--- script.js
+++ script.js
@@ -50,3 +50,31 @@ $("#rocketmeluncur").click(function () {
     self.className = "showrocket";
   }, 800);
 });
+
+var items = document.querySelectorAll(".custom-timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if (!items[i].classList.contains("in-view")) {
+        items[i].classList.add("in-view");
+      }
+    } else if (items[i].classList.contains("in-view")) {
+      items[i].classList.remove("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
